d14575ce2ef0dc652468873320e2b56fd4d0e099
shiragolda <shira_golda@yahoo.com>
2021-04-11 12:35:49 -0400

    Added support for laser locking with PCI-DAC board add-on

--- wavemeter_handler.py
+++ wavemeter_handler.py
@@ -5,13 +5,11 @@ import socket
 from ctypes import *
 
 import os
-#if __name__=='__main__':
-#    os.chdir("/home/labuser/Insync/electric.atoms@gmail.com/Google Drive/code/highfinesse_wavemeter")
 
-# wlmData.dll related imports
-import wlmData
-import wlmConst
 
+from wavemeter import WM
+
+from ast import literal_eval
 
 ##
 def get_ip():
@@ -24,63 +22,65 @@ def get_ip():
     finally:
         s.close()
     return IP
-    
+
     
 ##
 
 
 class wmHandler:
     def __init__(self,port=9000):
+        
+        self.wm = WM(mode='server')
+        
         zmq_context = zmq.Context()
         self.port = port
         
-        
-        try:
-            
-            self.socket = zmq_context.socket(zmq.REP)
-            self.socket.bind("tcp://*:%s"%self.port)
-            self.ip = get_ip()
 
-            print("Handling requests on %s:%s"%(self.ip,self.port))
-            print('')
-            
+        self.socket = zmq_context.socket(zmq.REP)
+        self.socket.bind("tcp://*:%s"%self.port)
+        self.ip = get_ip()
+
+        print("Handling requests on %s:%s"%(self.ip,self.port))
+        print('')
+        
+        go = True   
+       	while(go):
             try:
-                self.dll = wlmData.LoadDLL()
-                print("Loaded library")
-                time.sleep(1)
-            except Exception as e:
-                print("Error loading wlmData library")
-                print(e)
-            
-            
-            while(True):
-                self.handle()
+            	self.handle()
+            except(KeyboardInterrupt):
+            	go=False
+    
         
-        
-        except Exception as e:
-            if zmq.zmq_errno()==100:
-                print("Handler already started")
-            else:
-                print(e)
-                print('here')
         
     def handle(self):
-        message = self.receive()
+        msg = self.receive()
+        lst = msg.split(';')
+        func_name = lst[0]
+        func_args = literal_eval(lst[1])
+        func_kwargs = literal_eval(lst[2])
+        #print(func_args)
+        #print(func_kwargs)
         
-        returned = ""
-        loc = {'self':self}
-        
+        ret = ''
         try:
-            exec('returned = self.dll.%s'%message,{},loc) #loc passes the dictionary of local variables. neccesary because exec() doesn't return anything
-            returned = str(loc['returned'])
+        	func = getattr(self.wm,func_name)
+        	#print(func)
+
+        	ret = func(*func_args,**func_kwargs)
+        	#print(ret)
+
         except Exception as e:
-            print(e)
-        
-        self.reply(returned)
+        	print(e)
+
+        self.reply(str(ret))
+     
+    
+     
         
     def receive(self):
         message = self.socket.recv()
         if isinstance(message,bytes): message = message.decode()
+        #print(message)
         return message #string
 
     def reply(self,message):
